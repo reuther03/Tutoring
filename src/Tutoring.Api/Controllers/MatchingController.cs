6ea508477483c4b52eb832bd8734abf4711b560d
michaldrozd <michaldrozd03@gmail.com>
2024-04-25 03:22:11 +0200

    Added basic implementation for matching system

---
+++ src/Tutoring.Api/Controllers/MatchingController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tutoring.Api.Controllers.Base;
+using Tutoring.Application.Features.Matching.Query;
+
+namespace Tutoring.Api.Controllers;
+
+public class MatchingController : BaseController
+{
+    private readonly ISender _sender;
+
+    public MatchingController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchTutors([FromQuery] SearchTutorQuery query)
+    {
+        var result = await _sender.Send(query);
+        return HandleResult(result);
+    }
+}
---
+++ src/Tutoring.Application/Features/Matching/Query/SearchTutorQuery.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tutoring.Application.Abstractions;
+using Tutoring.Application.Abstractions.Database;
+using Tutoring.Application.Features.Users.Dto;
+using Tutoring.Common.Abstractions;
+using Tutoring.Common.Primitives;
+using Tutoring.Common.Primitives.Pagination;
+using Tutoring.Domain.Competences;
+using Tutoring.Domain.Users;
+
+namespace Tutoring.Application.Features.Matching.Query;
+
+public record SearchTutorQuery(int Page = 1, int PageSize = 10) : IQuery<PaginatedList<TutorDetailsDto>>
+{
+    internal sealed class Handler : IQueryHandler<SearchTutorQuery, PaginatedList<TutorDetailsDto>>
+    {
+        private readonly ITutoringDbContext _dbContext;
+        private readonly IUserContext _userContext;
+
+        public Handler(ITutoringDbContext dbContext, IUserContext userContext)
+        {
+            _dbContext = dbContext;
+            _userContext = userContext;
+        }
+
+
+        public async Task<Result<PaginatedList<TutorDetailsDto>>> Handle(SearchTutorQuery request, CancellationToken cancellationToken)
+        {
+            var userId = _userContext.UserId;
+            var user = await _dbContext.Users.OfType<Student>()
+                .Include(x => x.Subjects)
+                .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+            var subjects = user.Subjects.Select(x => x.Id).ToList();
+
+
+            var tutors = await _dbContext.Users.OfType<Tutor>()
+                .Include(x => x.CompetenceIds)
+                .Skip((request.Page - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Where(x => x.CompetenceIds.Any())
+                .AsQueryable()
+                .ToListAsync(cancellationToken);
+
+            if (tutors.Count != 0)
+            {
+                tutors.Where(x => x.CompetenceIds.Any(y => subjects.Contains(y))).ToList();
+            }
+
+            var totalTutors = await _dbContext.Users.OfType<Tutor>().CountAsync(cancellationToken);
+
+            return PaginatedList<TutorDetailsDto>.Create(request.Page, request.PageSize, totalTutors, tutors.Select(TutorDetailsDto.AsDto).ToList());
+        }
+    }
+}
