e9a479b763e738dab30e7a1f9a00937c3a7fa9e6
michaldrozd <michaldrozd03@gmail.com>
2024-05-03 18:05:33 +0200

    Delete unused method

--- src/Tutoring.Infrastructure/Database/Interceptors/ArchivingMatchInterceptor.cs
+++ src/Tutoring.Infrastructure/Database/Interceptors/ArchivingMatchInterceptor.cs
@@ -6,26 +6,12 @@ namespace Tutoring.Infrastructure.Database.Interceptors;
 
 public class ArchivingMatchInterceptor : SaveChangesInterceptor
 {
-    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
     {
-        var changeTracker = eventData.Context.ChangeTracker;
+        var changeTracker = eventData.Context?.ChangeTracker;
 
-        foreach (var entry in changeTracker.Entries<Matching>())
-        {
-            if (entry.State != EntityState.Modified) continue;
-
-            if (entry.Entity.IsArchived) continue;
-            entry.Entity.IsArchived = true;
-            entry.Entity.ArchivedAt = DateTime.UtcNow;
-        }
-
-        return base.SavingChanges(eventData, result);
-    }
-
-    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
-    {
-        var changeTracker = eventData.Context.ChangeTracker;
-
+        if (changeTracker == null) return base.SavingChangesAsync(eventData, result, cancellationToken);
         foreach (var entry in changeTracker.Entries<Matching>())
         {
             if (entry.State != EntityState.Modified) continue;
